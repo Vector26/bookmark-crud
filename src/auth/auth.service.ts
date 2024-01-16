f75a878f8772e2cb42da039cfaff927fdfc8f71a
vector26 <mrdev2604@gmail.com>
2024-01-16 22:55:27 +0530

    save game #1

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+    register(body: any) {
+        return('Method not implemented. register() is not implemented');
+    }
+    login(body: any) {
+        return('Method not implemented. login() is not implemented');
+    }
+}
